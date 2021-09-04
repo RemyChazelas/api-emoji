f48475df70bdb5b39d1fd9d9398753cb5f172cfb
Remy Chazelas <remychazelas@gmail.com>
2021-09-04 15:38:13 +0200

    add random

--- index.js
+++ index.js
@@ -1,81 +1,103 @@
-const Joi = require('joi');
+// const express = require('express');
+// const cors = require('cors');
+// const Joi = require('joi');
+// const emojis = require("./data.json");
+// const app = express();
+
+// app.use(cors());
+// app.use(express.json());
+
+// // GET ***************************************************
+
+
+
+// app.get('/', (req, res) => {
+//     const randomEmoji = emojis[Math.floor(Math.random() * emojis.length)];
+//     res.send(randomEmoji);
+// });
+
+// app.get('/api/emojis', (req, res) => {
+//     res.send(emojis);
+// });
+
+// app.get('/api/emojis/:id', (req, res) => {
+//     const emoji = emojis.find(c => c.id === parseInt(req.params.id));
+//     if (!emoji) return res.status(404).send('no emoji @ this ID')
+//     res.send(emoji);
+// });
+
+// // POST **************************************************
+
+// app.post('/api/emojis', (req, res) => {
+
+//     const { error } = validateEmoji(req.body);
+
+//     if (error) return res.status(400).send(error.details[0].message);
+
+//     const emoji = {
+//         id: emojis.length + 1,
+//         name: req.body.name
+//     };
+
+//     emojis.push(emoji);
+//     res.send(emoji);
+// })
+
+// // PUT ***************************************************
+
+// app.put('/api/emojis/:id', (req, res) => {
+//     const emoji = emojis.find(c => c.id === parseInt(req.params.id));
+//     if (!emoji) return res.status(404).send('no emoji @ this ID');
+
+//     const { error } = validateEmoji(req.body);
+
+//     if (error) return res.status(400).send(error.details[0].message);
+
+//     emoji.name = req.body.name;
+//     res.send(emoji);
+// });
+
+// // DELETE ************************************************
+
+// app.delete('/api/emojis/:id', (req, res) => {
+//     const emoji = emojis.find(c => c.id === parseInt(req.params.id));
+//     if (!emoji) return res.status(404).send('no emoji @ this ID');
+
+//     const index = emojis.indexOf(emoji);
+//     emojis.splice(index, 1);
+
+//     res.send(emoji);
+// });
+
+// function validateEmoji(emoji) {
+//     const schema = {
+//         name: Joi.string().min(3).required()
+//     };
+
+//     return Joi.validate(emoji, schema);
+// }
+
+// const port = 3000;
+
+// app.listen(port, () => { console.log(`listening on port http://localhost${port}`) });
+
 const express = require('express');
-const cors = require('cors');
-const emojis = require("./data.json");
 const app = express();
+const port = process.env.PORT || 3000;
+const emojis = require("./data.json");
+const cors = require('cors');
 
-const randomEmoji = emojis[Math.floor(Math.random() * data.length)];
 
-app.use(cors());
 app.use(express.json());
-
-function validateEmoji(emoji) {
-    const schema = {
-        name: Joi.string().min(3).required()
-    };
-
-    return Joi.validate(emoji, schema);
-}
-
-//GET***************************************************
+app.use(cors());
 
 app.get('/', (req, res) => {
-    res.send(randomEmoji)
+    const randomEmoji = emojis[Math.floor(Math.random() * emojis.length)];
+    res.send(randomEmoji);
 });
 
-app.get('/api/emojis', (req, res) => {
-    res.send(emojis)
+
+
+app.listen(port, () => {
+    console.log(`Application exemple à l'écoute sur le port ${port}!`)
 });
-
-app.get('/api/emojis/:id', (req, res) => {
-    const emoji = emojis.find(c => c.id === parseInt(req.params.id));
-    if (!emoji) return res.status(404).send('no emoji @ this ID')
-    res.send(emoji);
-});
-
-//POST**************************************************
-
-app.post('/api/emojis', (req, res) => {
-
-    const { error } = validateEmoji(req.body);
-
-    if (error) return res.status(400).send(error.details[0].message);
-
-    const emoji = {
-        id: emojis.length + 1,
-        name: req.body.name
-    };
-
-    emojis.push(emoji);
-    res.send(emoji);
-})
-
-//PUT***************************************************
-
-app.put('/api/emojis/:id', (req, res) => {
-    const emoji = emojis.find(c => c.id === parseInt(req.params.id));
-    if (!emoji) return res.status(404).send('no emoji @ this ID');
-
-    const { error } = validateEmoji(req.body);
-
-    if (error) return res.status(400).send(error.details[0].message);
-
-    emoji.name = req.body.name;
-    res.send(emoji);
-});
-
-//DELETE************************************************
-
-app.delete('/api/emojis/:id', (req, res) => {
-    const emoji = emojis.find(c => c.id === parseInt(req.params.id));
-    if (!emoji) return res.status(404).send('no emoji @ this ID');
-
-    const index = emojis.indexOf(emoji);
-    emojis.splice(index, 1);
-
-    res.send(emoji);
-});
-
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => console.log(`listening on port http://localhost${port}`));
